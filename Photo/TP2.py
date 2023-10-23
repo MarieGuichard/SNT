8f0bb8c4abaa5c9ea014fd90c3e8005a33c82508
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2023-10-23 17:41:50 +0200

    Update TP2.py

--- Photo/TP2.py
+++ Photo/TP2.py
@@ -11,13 +11,4 @@ def FiltreRouge(NomFichier):
     img.show() # affiche l'image modifiée
     
 def FiltreVert(NomFichier):
-    img = Image.open(NomFichier) #chargement de l'image
-    Largeur, Hauteur = img.size # récupération de ses dimensions
-    
-    #double boucle pour parcourir tous les pixels de l'image
-    for x in range(Largeur): # boucle sur les colonnes de l'image
-        for y in range(Hauteur): #boucle sur les lignes de l'image
-            r,v,b, = img.getpixel((x,y))
-            img.putpixel((x,y),(r,0,0))
-    img.show() # affiche l'image modifiée
-    
+    pass
