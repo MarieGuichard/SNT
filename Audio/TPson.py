4bcbb38fb984a2fb0a47305cec914618b7949ec6
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2024-04-02 16:24:53 +0200

    Update and rename TPson to TPson.py

---
+++ Audio/TPson.py
@@ -0,0 +1,34 @@
+import winsound
+import wave
+
+def notes1():
+    pass
+
+
+
+
+
+def notes2():
+    pass
+
+
+
+
+
+
+def notes3():
+    pass
+
+
+
+
+
+def notes4():
+    pass
+
+
+
+
+
+def melodie():
+    pass
